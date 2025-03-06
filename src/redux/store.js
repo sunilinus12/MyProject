84a96e68cd367577ce2737351f48a8e9d3950ad7
sunil jaunwal <sunil@hipster-inc.com>
2025-03-06 16:11:10 +0530

    added default middle ware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./UserSlice";
+// import logger from "redux-logger"; // Import logger middleware
 
+// Custom Middleware
+const customMiddleware = (store) => (next) => (action) => {
+  console.log("Custom Middleware: Dispatching", action);
+  let result = next(action); // Pass action to next middleware/reducer
+  console.log("Custom Middleware: Next State", store.getState());
+  return result;
+};
 const store = configureStore({
   reducer: {
     user: userReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(customMiddleware), // No logger, only custom middleware
 });
+
 
 export default store;
